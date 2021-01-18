2784a4a6c24e7c596f1dab7859d9e3e80a401f0f
bluekim <ohan09@naver.com>
2021-01-18 22:33:22 +0900

    test router

--- backend/router/test.js
+++ backend/router/test.js
@@ -5,8 +5,8 @@ const elasticService = require('../service/elasticService');
 function router (app){
     app.get('/test/dart', async (req, res) => {
         console.log('fetch test/dart');
-        const obj = await dartStockService.updateFinancialStatements();
-        res.send(obj);
+        await dartStockService.updateFinancialStatements();
+        res.send('fetch test/dart');
     })
 };
 
